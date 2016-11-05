bc77902c05d6223f0e3cd168b33c7e84646b70fa
Vismar <exanimoteam@gmail.com>
2016-11-05 18:24:24 +0300

    Issue #12
    Show error window if configuration manager can not be initialized. #12

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,25 @@
 #include "./Utils/Config/ConfigurationManager.hpp"
 #include "./Utils/UserData/UserData.hpp"
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
     if (ConfigurationManager::Instance().Initialize())
     {
         UserData::Instance().Initialize();
-        QApplication a(argc, argv);
         Ui::MainWindow w;
         w.show();
 
         return a.exec();
     }
+    // If configuration manager do not initialized, just show a error message.
     else
     {
-        // Should be replaced by error window!
-        qDebug() << "Configuration manager can not be initialized!\n";
+        QMessageBox msgBox;
+        msgBox.setText("Configuration manager can not be initialized! Check your config.cfg file!");
+        msgBox.exec();
     }
 
     return -1;
