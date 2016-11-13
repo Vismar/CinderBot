584c518248e19caaeee529d3bd432d283543f6b8
Vismar <exanimoteam@gmail.com>
2016-11-14 01:38:56 +0300

    #15 - Configuration manager now use xml file
    New defines in Config parameters, old defines were updated.
    Configuration manager now use xml file (config.xml). XML file included.

--- src/Utils/Config/ConfigurationManager.cpp
+++ src/Utils/Config/ConfigurationManager.cpp
@@ -3,6 +3,8 @@
 #include <QFile>
 #include <QTextStream>
 
+#include <QDebug>
+
 #define CONFIG_READER_FILE_PATH "./data/config/"
 #define CONFIG_READER_FILE_NAME "config.cfg"
 #define CONFIG_READER_COMMENT '#'
@@ -14,43 +16,40 @@ ConfigurationManager& ConfigurationManager::Instance()
     return instance;
 }
 
-bool ConfigurationManager::Initialize()
+QString ConfigurationManager::Initialize()
 {
-    bool result(true);
-    QString line;
-    QString fileName = CONFIG_READER_FILE_PATH;
-    fileName.append(CONFIG_READER_FILE_NAME);
-    QFile configFile(fileName);
+    QString error;
+    QFile configFile("./data/config/config.xml");
     if (configFile.open(QIODevice::ReadOnly))
     {
-        QTextStream in(&configFile);
-        while (!in.atEnd())
+        _xmlReader.setDevice(&configFile);
+
+        while (!_xmlReader.atEnd())
         {
-            line = in.readLine();
-            if (line.at(0) != CONFIG_READER_COMMENT)
+            _xmlReader.readNext();
+            if (_xmlReader.isStartElement())
             {
-                int separator = line.indexOf(CONFIG_READER_SEPARATOR);
-                if (separator != -1)
+                if(_xmlReader.name() == CFGS_LOGIN)
                 {
-                    _params.insert(line.left(separator),
-                                   line.right(line.length() - (separator + 1)));
+                    _ReadLoginData();
+                }
+                if (_xmlReader.name() == CFGS_CONFIG)
+                {
+                    _ReadConfigData();
                 }
             }
+        }
+        if (_xmlReader.hasError())
+        {
+            error = _xmlReader.errorString();
         }
     }
     else
     {
-        result = false;
-    }
-    configFile.close();
-    if (!_params.contains(CONFIG_LOGIN_NAME) &&
-        !_params.contains(CONFIG_LOGIN_OATH_KEY) &&
-        !_params.contains(CONFIG_LOGIN_CHANNEL))
-    {
-        result = false;
+        error = "No config xml file!";
     }
 
-    return result;
+    return error;
 }
 
 bool ConfigurationManager::GetStringParam(QString parameter, QString& value)
@@ -62,4 +61,109 @@ bool ConfigurationManager::GetStringParam(QString parameter, QString& value)
     }
 
     return result;
+}
+
+void ConfigurationManager::_ReadLoginData()
+{
+    while (!_xmlReader.atEnd())
+    {
+        _xmlReader.readNext();
+        if (_xmlReader.isEndElement())
+        {
+            if (_xmlReader.name() == CFGS_LOGIN)
+            {
+                break;
+            }
+        }
+        if (_xmlReader.isStartElement())
+        {
+            _params.insert(_xmlReader.name().toString(), _xmlReader.readElementText());
+        }
+    }
+}
+
+void ConfigurationManager::_ReadConfigData()
+{
+    while (!_xmlReader.atEnd())
+    {
+        _xmlReader.readNext();
+        if (_xmlReader.isEndElement())
+        {
+            if (_xmlReader.name() == CFGS_CONFIG)
+            {
+                break;
+            }
+        }
+        if (_xmlReader.isStartElement())
+        {
+            if (_xmlReader.name() == CFGS_CURRENCY)
+            {
+                _params.insert(_xmlReader.name().toString(), _xmlReader.readElementText());
+            }
+            else if (_xmlReader.name() == CFGS_IGNORE)
+            {
+                _ReadIgnoreList();
+            }
+            else if (_xmlReader.name() == CFGS_COVENANTS)
+            {
+                _ReadCovenantList();
+            }
+        }
+    }
+}
+
+void ConfigurationManager::_ReadIgnoreList()
+{
+    QString ignoreList;
+    while (!_xmlReader.atEnd())
+    {
+        _xmlReader.readNext();
+        if (_xmlReader.isEndElement())
+        {
+            if (_xmlReader.name() == CFGS_IGNORE)
+            {
+                break;
+            }
+        }
+        if (_xmlReader.isStartElement())
+        {
+            if (_xmlReader.name() == CFGS_USER)
+            {
+                if (!ignoreList.isEmpty())
+                {
+                    ignoreList.append(',');
+                }
+                ignoreList.append(_xmlReader.readElementText());
+            }
+        }
+    }
+    _params.insert("IgnoreList", ignoreList);
+}
+
+void ConfigurationManager::_ReadCovenantList()
+{
+    QString covenants;
+    while (!_xmlReader.atEnd())
+    {
+        _xmlReader.readNext();
+        if (_xmlReader.isEndElement())
+        {
+            if (_xmlReader.name() == CFGS_COVENANTS)
+            {
+                break;
+            }
+        }
+        if (_xmlReader.isStartElement())
+        {
+            if (_xmlReader.name() == CFGS_COVENANT)
+            {
+                if (!covenants.isEmpty())
+                {
+                    covenants.append(',');
+                }
+                covenants.append(_xmlReader.readElementText());
+            }
+        }
+    }
+    _params.insert("CovenantList", covenants);
 }
