7005692f5feda2f18d82b39d10fcf44449cdada1
Vismar <exanimoteam@gmail.com>
2016-11-19 01:38:16 +0300

    #15 User data now use .xml file
    UserData stores in xml file in "./data/data/UserData.xml"

--- src/Utils/UserData/UserData.hpp
+++ src/Utils/UserData/UserData.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <QString>
 #include <QHash>
+#include <QXmlStreamReader>
 
 enum UserDataParam
 {
-    UDP_Messages,
+    UDP_Messages = 0,
     UDP_Currency,
     UDP_Covenant,
     UDP_End
@@ -16,16 +17,17 @@ private:
     QHash<QString, QHash<QString, QString> > _userData;
     QHash<QString, QString>                  _defaultParams;
 
-    void _SetUserData(const QString& userName,
-                      const QString& rawLine);
-    void _SetDefaultUserData(const QString& userName,
-                             const QHash<QString, QString>& params);
-    void _ResetToDefaultUserData(const QString& userName);
-    void _AddUserData(const QString& userName,
-                      const QHash<QString, QString>& params);
-    void _InitializeDefaultUserData();
+    QXmlStreamReader _xmlReader;
+    QXmlStreamWriter _xmlWriter;
 
     QString _GetUDPParam(UserDataParam UDP);
+    void _AddUserData(const QString& userName, const QHash<QString, QString>& params);
+    void _ResetToDefaultUserData(const QString& userName);
+    void _InitializeDefaultUserData();
+
+    QHash<QString, QString> _ReadUserParams();
+    void _ReadUserData();
+    void _WriteUserData();
 
 public:
     static UserData& Instance();
@@ -33,9 +35,6 @@ public:
 
     void Initialize();
 
-    QString GetUserDataParam(const QString& userName,
-                             UserDataParam UDP);
-    void UpdateUserData(const QString& userName,
-                        UserDataParam UDP,
-                        const QString& newValue);
+    QString GetUserDataParam(const QString& userName, UserDataParam UDP);
+    void UpdateUserData(const QString& userName, UserDataParam UDP, const QString& newValue);
 };
