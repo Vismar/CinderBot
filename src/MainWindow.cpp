3ca11d54988d515e4a4f08b3e2d0ae145c8e01e8
Vismar <exanimoteam@gmail.com>
2016-11-20 15:41:08 +0300

    #1  Commenctaries for MainWindow
    Commenctaries for MainWindow

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -4,11 +4,16 @@ using namespace Ui;
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
+    // Initialize UI
     InitUi();
 
+    // Create chat widget
     _chat = new ChatWidget(this);
+    // Set chat widget parameters
+    // TODO: May be constant size should be changed to variable
     _chat->setFixedSize(300, 400);
 
+    // Initialize twitch client
     _twitchClient = new TwitchClient(this);
     connect(_twitchClient, &TwitchClient::NewMessage,
             _chat, &ChatWidget::AddEntry);
@@ -20,6 +25,9 @@ MainWindow::~MainWindow()
 
 void MainWindow::InitUi()
 {
+    // Title of app
     this->setWindowTitle("Cinder Bot");
+    // Size of app
+    // TODO: Should be changed to resizeable
     this->setFixedSize(640, 400);
 }
--- src/MainWindow.hpp
+++ src/MainWindow.hpp
@@ -6,17 +6,31 @@
 namespace Ui
 {
 
+/*!
+ * \class MainWindow
+ *
+ * The MainWindow class contains all widgets and TwtichClient
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 private:
+    /*!
+     * Scrollable chat widget
+     */
     ChatWidget*   _chat;
+    /*!
+     * Twitch client. Handles all network stuff.
+     */
     TwitchClient* _twitchClient;
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    /*!
+     * Initialize UI stuff.
+     */
     void InitUi();
 };
 
