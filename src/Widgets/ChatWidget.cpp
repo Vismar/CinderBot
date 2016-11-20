18d049b52d8790b0de7714130950573fe507d5d3
Vismar <exanimoteam@gmail.com>
2016-11-20 20:27:08 +0300

    #3 commentaries
    Commentaries for ChatWidget

--- src/Widgets/ChatWidget.cpp
+++ src/Widgets/ChatWidget.cpp
@@ -10,6 +10,7 @@ using namespace Ui;
 
 ChatWidget::ChatWidget(QWidget *parent) : QScrollArea(parent)
 {
+    // Set params to layout
     setWidgetResizable(true);
     _container = new QWidget();
     this->setWidget(_container);
@@ -18,15 +19,15 @@ ChatWidget::ChatWidget(QWidget *parent) : QScrollArea(parent)
     _layout->setMargin(10);
 }
 
-ChatWidget::~ChatWidget()
-{
-}
+ChatWidget::~ChatWidget() {}
 
 void ChatWidget::AddEntry(ChatMessage message, bool botMessage)
 {
     Q_UNUSED(botMessage);
+    // Check if we already have maximum number of entries
     if (_layout->count() == MAX_NUMBER_OF_ENTRIES)
     {
+        // Remove oldest entry
         QLayoutItem* entry;
         if ((entry = _layout->takeAt(0)) != 0)
         {
@@ -34,18 +35,22 @@ void ChatWidget::AddEntry(ChatMessage message, bool botMessage)
             delete entry;
         }
     }
+    // Add new entry
     ChatMessageWidget* newEntry = new ChatMessageWidget("");
     newEntry->insertHtml("<b>" + message.GetColor() + message.GetAuthor() + " | "
                          + message.GetTimeStamp() + "</b></font><hr>"
                          + message.GetMessage());
     _layout->addWidget(newEntry);
+    // If scroll area at the bottom, it should be scrolled down, otherwise it will go up.
     if (verticalScrollBar()->value() == verticalScrollBar()->maximum())
     {
+        // Because of delay we should wait a little bit to scroll
         QTimer::singleShot(100, this, SLOT(_AutoScrollDown()));
     }
 }
 
 void ChatWidget::_AutoScrollDown()
 {
+    // Scroll to the botton of scroll area
     verticalScrollBar()->setValue(verticalScrollBar()->maximum());
 }
