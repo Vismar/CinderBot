44981cbbf4ca5d2c32671bdf53f48e22204e275f
Vismar <exanimoteam@gmail.com>
2016-11-21 21:43:02 +0300

    #6 commentaries
    BotAI commentaries and small fix

--- src/AI/BotAI.hpp
+++ src/AI/BotAI.hpp
@@ -5,16 +5,40 @@
 #include "../Chat/ChatMessage.hpp"
 #include "./ChatCommands/BaseChatCommand.hpp"
 
+/*!
+ * Class BotAI
+ * Stores commands and handles its execute
+ */
 class BotAI : public QObject
 {
     Q_OBJECT
 private:
+    /*! Array of commands */
     QVector<BaseChatCommand*> _chatCommands;
+    /*! Ignore list. Commands from users that stored in this list will not be executed */
     QStringList               _ignoreList;
 
+    /*!
+     * Check if user is in ignore list
+     * \param(IN) userName - name of a user
+     * \return true if user is in ignore list
+     */
     bool _CheckIgnoreList(const QString& userName);
+    /*!
+     * Update user data (Message counter and currency)
+     * \param(IN) message - chat message
+     */
     void _UpdateUserData(const ChatMessage& message);
+    /*!
+     * Increment message counter
+     * \param(IN) userName - name of a user
+     */
     void _IncrementMessageCounter(const QString& userName);
+    /*!
+     * Add currency to a user
+     * \param(IN) userName - name of a user
+     * \param(IN) value - amount of currency that should be added
+     */
     void _AddCurrency(const QString& userName, const int value);
 
 public:
@@ -22,8 +46,17 @@ public:
     ~BotAI() {}
 
 signals:
+    /*!
+     * Signal about new bot message
+     * \param(IN) message - bot message
+     */
     void NewBotMessage(QString message);
 
 public slots:
+    /*!
+     * Read and process new message
+     * \param(IN) message - chat message
+     * \param(IN) botMessage - is it a bot or not?
+     */
     void ReadNewMessage(ChatMessage message, bool botMessage);
 };
