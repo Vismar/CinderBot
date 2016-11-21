2e436b11acca2e8762489b2a9aba2d7998b36181
Vismar <exanimoteam@gmail.com>
2016-11-21 21:48:44 +0300

    #7 commenatries
    BaseChatCommand commentaries

--- src/AI/ChatCommands/BaseChatCommand.hpp
+++ src/AI/ChatCommands/BaseChatCommand.hpp
@@ -2,10 +2,20 @@
 #include <QString>
 #include "../../Chat/ChatMessage.hpp"
 
+/*!
+ * Class BaseChatCommand
+ * Base class to all commands for bot
+ */
 class BaseChatCommand
 {
 public:
     virtual ~BaseChatCommand() {}
+    /*!
+     * Trying to execute command if it was founded in message and return result
+     * \param(IN) message - chat message
+     * \param(OUT) answer - answer of command if it was executed
+     * \return true if command was executed
+     */
     virtual bool GetAnswer(ChatMessage& message, QString& answer)
     {
         Q_UNUSED(message);
