e330ba443f813982674387b6f00deb9c22d939fa
Vismar <exanimoteam@gmail.com>
2016-11-21 22:04:50 +0300

    #9 commentaries
    UserDataChatCommand commentaries

--- src/AI/ChatCommands/UserDataChatCommand.hpp
+++ src/AI/ChatCommands/UserDataChatCommand.hpp
@@ -1,8 +1,14 @@
 #pragma once
 #include "BaseChatCommand.hpp"
 
+/*!
+ * Class UserDataChatCommand
+ * Handles user data related commands
+ */
 class UserDataChatCommand : public BaseChatCommand
 {
 public:
+    ////////////////////////////////
+    /// BaseChatCommand override
     bool GetAnswer(ChatMessage& message, QString& answer);
 };
