06834f9af6120c7dcb4cbd1bf5cc65656a89fd12
Ilya Lobanov <exanimoteam@gmail.com>
2017-06-06 00:15:36 +0300

    Issue-#97: Covenants - Levels for covenants
    Now covenants in db have exp and level fields

--- src/AI/ChatCommands/CovenantCommands/InfoCovenantCommand.cpp
+++ src/AI/ChatCommands/CovenantCommands/InfoCovenantCommand.cpp
@@ -24,7 +24,9 @@ InfoCovenantCommand::InfoCovenantCommand()
 void InfoCovenantCommand::Initialize()
 {
     _name = "!cov_info";
-    _answers.push_back("Covenant: COV_NAME; Members: COV_MEMBERS; Commands: COV_CMD_NUM;");
+    _answers.push_back("Covenant: COV_NAME; Leader: COV_LEADER; "
+                       "Members: COV_MEMBERS; Commands: COV_CMD_NUM; "
+                       "Level: COV_LEVEL; Experience: COV_EXP/COV_NEED_EXP;");
     _answers.push_back("Description: ");
     _answers.push_back("Please provide name of the covenant, @.");
 }
@@ -46,7 +48,11 @@ void InfoCovenantCommand::_GetAnswer(const ChatMessage& message, QStringList& an
             if (query->first())
             {
                 QString temp = _answers.at(MSG_BASE_INFO);
+                // Covenant name
                 temp.replace("COV_NAME", covenant);
+                // Leader of covenant
+                temp.replace("COV_LEADER", query->value("Leader").toString());
+                // Number of members
                 DB_QUERY_PTR memberQuery = DB_SELECT("UserData", "COUNT(*)",
                                                                  QString("Covenant = '%1'").arg(covenant));
                 if (memberQuery != NULL)
@@ -56,8 +62,18 @@ void InfoCovenantCommand::_GetAnswer(const ChatMessage& message, QStringList& an
                         temp.replace("COV_MEMBERS", (memberQuery->value(0).toString()));
                     }
                 }
+                // Number of available commands
                 temp.replace("COV_CMD_NUM", query->value("CmdSlots").toString());
+                // Covenant level
+                temp.replace("COV_LEVEL", query->value("Level").toString());
+                // Covenant current exp
+                temp.replace("COV_EXP", query->value("Exp").toString());
+                // Covenant needed exp
+                temp.replace("COV_NEED_EXP", QString::number(query->value("Level").toInt()*1000));
+
+                // Add result to answer
                 answer.append(temp);
+                // Get description
                 temp = query->value("Description").toString();
                 if (!temp.isEmpty())
                 {
