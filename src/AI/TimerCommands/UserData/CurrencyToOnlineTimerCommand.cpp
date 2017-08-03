f7d6d9ba608756efdb03bc7ff6327ef7f6e5f504
Vismar <exanimoteam@gmail.com>
2017-08-04 01:29:29 +0300

    US-172: Timer command - Currency per time
    Now we have timer commands! And especially - currency timer command.

---
+++ src/AI/TimerCommands/UserData/CurrencyToOnlineTimerCommand.cpp
@@ -0,0 +1,85 @@
+/*************************************************************************
+***************  CinderBot - standalone bot for Twitch.tv ****************
+******** Copyright (C) 2017  Ilya Lobanov (exanimoteam@gmail.com) ********
+********         Check full copyright header in main.cpp          ********
+**************************************************************************/
+#include "CurrencyToOnlineTimerCommand.hpp"
+#include "Utils/Config/ConfigurationManager.hpp"
+#include "Utils/UserData/RealTimeUserData.hpp"
+#include "Utils/UserData/UserData.hpp"
+
+using namespace Utils::Configuration;
+using namespace TimerCommand::UserDataTimerCmd;
+
+#define DEFAULT_TIMER_VALUE    60000
+#define DEFAULT_CURRENCY_VALUE "1"
+
+///////////////////////////////////////////////////////////////////////////
+
+CurrencyToOnlineTimerCommand::CurrencyToOnlineTimerCommand()
+{
+    connect(&ConfigurationManager::Instance(), &ConfigurationManager::ParameterChanged,
+            this, &CurrencyToOnlineTimerCommand::OnCfgParamChanged);
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+CurrencyToOnlineTimerCommand::~CurrencyToOnlineTimerCommand() { }
+
+///////////////////////////////////////////////////////////////////////////
+
+void CurrencyToOnlineTimerCommand::OnCfgParamChanged(Utils::Configuration::CfgParam cfgParam)
+{
+    if (cfgParam == CurrencyTimer)
+    {
+        _UpdateTimer();
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+void CurrencyToOnlineTimerCommand::_UpdateTimer()
+{
+    QString param;
+    ConfigurationManager::Instance().GetStringParam(CurrencyTimer, param);
+    int timerValue = param.toInt();
+    if (timerValue <= 0)
+    {
+        timerValue = DEFAULT_TIMER_VALUE;
+    }
+    _timer.start(timerValue);
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+void CurrencyToOnlineTimerCommand::_TimerAction()
+{
+    // Set value of currency to give
+    QString currencyToGive = DEFAULT_CURRENCY_VALUE;
+    ConfigurationManager::Instance().GetStringParam(CurrencyOverTime, currencyToGive);
+
+    const QStringList &userList = RealTimeUserData::Instance()->GetUserList();
+    for (int i = 0; i < userList.count(); ++i)
+    {
+        QString ignoreList;
+        ConfigurationManager::Instance().GetStringParam(IgnoreList, ignoreList);
+        if (!ignoreList.contains(userList[i]))
+        {
+            // Get currency new value
+            QString currencyValue = UD_GET_PARAM(userList[i], UDP_Currency);
+            int currencyNewValue = currencyValue.toInt() + currencyToGive.toInt();
+            if (currencyNewValue < 0)
+            {
+                currencyNewValue  = 0;
+            }
+
+            // Update value
+            currencyValue = QString::number(currencyNewValue);
+
+            // Set new value
+            UD_UPDATE(userList[i], UDP_Currency, currencyValue);
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////
---
+++ src/AI/TimerCommands/UserData/CurrencyToOnlineTimerCommand.hpp
@@ -0,0 +1,50 @@
+/*************************************************************************
+***************  CinderBot - standalone bot for Twitch.tv ****************
+******** Copyright (C) 2017  Ilya Lobanov (exanimoteam@gmail.com) ********
+********         Check full copyright header in main.cpp          ********
+**************************************************************************/
+#pragma once
+#include "Ai/TimerCommands/BaseTimerCommand.hpp"
+#include "Utils/Config/ConfigurationParameters.hpp"
+
+/*!
+ * \brief Contains all timer command related.
+ */
+namespace TimerCommand
+{
+/*!
+ * \brief Contains all user data timer commands.
+ */
+namespace UserDataTimerCmd
+{
+
+/*!
+ * \brief Timer command to give currency on timer.
+ *
+ * Gives specified in configuration manager amount of currency to all users in chat.
+ */
+class CurrencyToOnlineTimerCommand : public BaseTimerCommand
+{
+    Q_OBJECT
+public:
+    CurrencyToOnlineTimerCommand();
+    ~CurrencyToOnlineTimerCommand();
+
+public slots:
+    /*!
+     * \brief Updates timer if cfgParam == CurrencyTimer.
+     * \param cfgParam - configuration paramter.
+     *
+     * Get new value of CurrencyTimer and use it to restart timer.
+     */
+    void OnCfgParamChanged(Utils::Configuration::CfgParam cfgParam);
+
+protected:
+    ////////////////////////////////
+    /// BaseTimerCommand override
+    void _UpdateTimer();
+    void _TimerAction();
+};
+
+}
+}
